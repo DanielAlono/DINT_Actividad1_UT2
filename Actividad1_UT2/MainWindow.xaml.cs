0d2da00ee8be059fcbe1c8cec5847c173584edcf
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2020-10-20 12:25:28 +0200

    Agregar archivos de proyecto.

---
+++ Actividad1_UT2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace Actividad1_UT2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int numeroRandom;
+        Random generador = new Random();
+        public MainWindow()
+        {
+            numeroRandom = generador.Next(0, 101);
+            InitializeComponent();
+        }
+
+        private void comprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(numeroTextBox.Text) == numeroRandom) resultadoTextBlock.Text = "¡Has acertado!";
+            else if (int.Parse(numeroTextBox.Text) < numeroRandom) resultadoTextBlock.Text = "Te has quedado corto";
+            else resultadoTextBlock.Text = "Te has pasado";
+        }
+
+        private void reiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            numeroRandom = generador.Next(0, 101);
+            resultadoTextBlock.Text = "";
+            numeroTextBox.Text = "";
+        }
+    }
+}
